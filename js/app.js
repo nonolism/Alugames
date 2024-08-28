b8b854b51c0df022fca3bdd4229d4630eef3c598
Nonolism <ruanvitormoreno@outlook.com>
2024-08-28 17:16:31 -0300

    Adicionar função para informar quantidade de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+quantidadeAlugados();
+
 function alterarStatus(item){
     let jogo = document.getElementById(`game-${item}`);
     let imagem = jogo.querySelector('div');
@@ -15,9 +17,20 @@ function alterarStatus(item){
         botao.classList.add('dashboard__item__button--return');
         botao.innerText = 'Devolver';
     }
+    quantidadeAlugados();
 } 
 
 function confirmarDevolucao(){
     let confirmar = confirm('Você tem certeza que deseja devolver o jogo?');
     return confirmar;
+}
+
+function quantidadeAlugados(){
+    quantidade = document.getElementsByClassName('dashboard__item__img--rented').length;
+    if (quantidade > 0){
+        texto = quantidade > 1 ? `Existem atualmente ${quantidade} Jogos alugados` : `Existe atualmente ${quantidade} Jogo alugado`;
+    } else {
+        texto = 'Nenhum jogo foi alugado ainda';
+    }
+    console.log(texto);
 }
